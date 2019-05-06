316735c3e3249a6c5b436729ec2b10c3e2adba6d
juliomilani <49495322+juliomilani@users.noreply.github.com>
2019-05-06 15:16:27 -0300

    Update key_reader.c

--- key_reader.c
+++ key_reader.c
@@ -2,7 +2,7 @@
 //Julio Milani de Lucena (243876)
 //Lucas Meurer Lameira (243875)
 
-//Essa funcao ler o botao pressionado por interrupcao
+//Essa funcao ler o botao pressionado
 
 //#include <reg51.h>
 #include <stdio.h>
@@ -17,16 +17,12 @@ unsigned char keypad[4][3] =	{{'1','2','3'},
 															{'7','8','9'},
 															{'*','0','#'}};
 
-
 void delay(){
     unsigned short a;
     for(a=0;a<100;a++);
 }
 
-
-
-char debounce(char linha, char coluna)
-{
+char debounce(char linha, char coluna){
     char count = 0;
     char key_last = 0;	
     char key_now;
@@ -53,7 +49,6 @@ char debounce(char linha, char coluna)
 char key_detect(){
     unsigned char linha = 0;
     unsigned char coluna = 0;
-
     for(linha=0;linha<4;linha++){
         for(coluna=0;coluna<3;coluna++){
             if(!debounce(linha,coluna)){
@@ -63,52 +58,14 @@ char key_detect(){
         }
     }
 
-
-    return 0;
+   return 0;
 }
-/*
-char key_detect2(){
-    unsigned char linha = 0;
-    unsigned char col = 0;
-    for(linha=0;linha++;linha<4){
-        porta0 = ~(1<<(7-linha));
-				for(col=0;col++;col<4){ //find pressed column
-				    if (~(porta0 & (1<<col))){
-								delay();
-								while(~(porta0 & (1<<col)));
-                //return keypad[linha][col];
-								return 1;
-            }
-				}
-    }
-    return 0;
-}*/
-
-unsigned char i;
 
 void main(){
-
     while(1){
-			/*
-			char row = 0;
-			char col = 0;
-			for(row=0;row<4;row++){
-				for(col=0;col<3;col++){
-					if(!debounce(row,col)){
-								while(!debounce(row,col)){}
-								LED = !LED;							
-					}
-				}
-			}*/
-					
-			
-
 			key_pressed = key_detect();
 			if(key_pressed=='6'||key_pressed=='5'){
 				LED = !LED;
 			}
-
 		}
-			
 }
-
